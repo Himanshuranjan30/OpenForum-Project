e5e84dbbc290b875303338503829375649d492ef
Himanshu <himanshuranjan30@gmail.com>
2021-03-10 16:09:20 +0530

    added engines

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 80,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI
   }
