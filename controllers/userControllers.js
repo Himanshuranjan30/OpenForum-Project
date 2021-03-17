d686e226ac5d06a9fc628365ac8aaa3381b8a924
Himanshu <himanshuranjan30@gmail.com>
2021-03-17 23:17:44 +0530

    bucket added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -23,7 +23,7 @@ const uploadaimage=(req,res)=>{
     if (perr) {
       console.log("Error uploading data: ", perr);
     } else {
-      User.updateOne({id:req.query.id},{
+      User.findByIdAndUpdate(req.query.id,{
         photo:pres["location"]
       }).exec()
       res.send(pres)
