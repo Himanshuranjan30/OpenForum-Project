d7ef0a210b49ffd2cffc6f6f9c012817f479f3f2
Himanshu <himanshuranjan30@gmail.com>
2021-03-17 11:22:45 +0530

    cors tweak

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -22,8 +22,8 @@ passport.deserializeUser(function (obj, cb) {
 passport.use(
   new GoogleStrategy(
     {
-      clientID:config.clientid,
-      clientSecret: config.clientsecret,
+      clientID:"789965715216-2sbi4nk44kbaabtsqt7vlddgklieksq9.apps.googleusercontent.com",
+      clientSecret: "Jtdl2O8dBSJnTUrJ9I6UEBhf",
       callbackURL: "https://openforumsocial.herokuapp.com/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
